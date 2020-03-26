86fba55e8aee0834d564202b1198097c1942273a
Topi Tuulensuu <topi.tuulensuu@kone.com>
2020-03-26 13:38:29 +0200

    corona.sh: add linux support

--- scripts/corona.sh
+++ scripts/corona.sh
@@ -2,7 +2,16 @@
 URL="https://w3qa5ydb4l.execute-api.eu-west-1.amazonaws.com/prod/finnishCoronaData"
 FILE="$HOME/.cache/corona"
 
-[[ $(stat -t '%y-%m-%d' $FILE 2>/dev/null | cut -d' ' -f10 | sed 's/"//g') != $(date -u +"%y-%m-%d") ]] &&
-    curl -s $URL > $FILE
+if [[ $(uname -s) = "Linux" ]]; then
+    TODAY=$(date +"%Y-%m-%d")
+    STAT=$(stat --format='%y-%m-%d' $FILE | cut -d' ' -f1)
+else
+    TODAY=$(date -u +"%y-%m-%d")
+    STAT=$(stat -t '%y-%m-%d' $FILE 2>/dev/null | cut -d' ' -f10 | sed 's/"//g')
+fi
+
+[[ $TODAY != $STAT ]] &&
+    curl -s $URL > $FILE &&
+    date >> $HOME/.corona_time
 
 corona_parser.py $FILE $1
