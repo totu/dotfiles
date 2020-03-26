2f4ceaefdaf96c4cec3db71a9aa5823f87386233
Topi Tuulensuu <topi.tuulensuu@kone.com>
2020-03-26 13:19:20 +0200

    corona_parser: delta to yesterday and recovered today count

--- scripts/corona_parser.py
+++ scripts/corona_parser.py
@@ -8,29 +8,44 @@ TODAY = datetime.datetime.now().strftime("%Y-%m-%d")
 
 def parse_data(data):
     global TODAY
+    YESTERDAY = TODAY.split("-")
+    YESTERDAY = "-".join([YESTERDAY[0], YESTERDAY[1], str(int(YESTERDAY[2]) + 1)])
     count = len(data)
     today = len([x for x in data if x["date"].split("T")[0] == TODAY])
-    return count, today
+    yesterday = len([x for x in data if x["date"].split("T")[0] == YESTERDAY])
+    return count, today, yesterday
 
 
 def main(data, colors):
     # Confirmed cases
-    confirmed, confirmed_today = parse_data(data["confirmed"])
+    confirmed, confirmed_today, confirmed_yesterday = parse_data(data["confirmed"])
 
     # Deaths
     deaths = len(data["deaths"])
     mortality = deaths / confirmed
 
     # Recoveries
-    _, recovered_today = parse_data(data["recovered"])
+    _, recovered_today, recovered_yesterday = parse_data(data["recovered"])
 
     # Case delta count
-    delta = confirmed_today - recovered_today
+    delta = "▼" if confirmed_yesterday - confirmed_today < 0 else "▲"
 
     # Figure out color for delta
-    delta_color = colors.green if delta < 1 else colors.red
+    delta_color = colors.green if confirmed_yesterday - confirmed_today < 0 else colors.red
 
-    out = "😷 %s (%s%s%s) 💀 %s" % (confirmed, delta_color, delta, colors.end, deaths)
+    out = "😷%s%s%s%s (%s+%s%s/%s-%s%s) 💀%s" % (
+        confirmed,
+        delta_color,
+        delta,
+        colors.end,
+        colors.red,
+        confirmed_today,
+        colors.end,
+        colors.green,
+        recovered_today,
+        colors.end,
+        deaths,
+    )
     if mortality > 0.1:
         out += " (%0.1f%s)" % (mortality, "%")
 
